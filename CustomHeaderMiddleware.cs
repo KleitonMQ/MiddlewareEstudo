424a3d85b3af9f3668838e925a53773b91e8f7ee
Kleiton MQ <kleiton.x5@gmail.com>
2023-03-01 11:03:26 -0300

    inclusao da classe de customheader e configuracao do logger

---
+++ CustomHeaderMiddleware.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareEstudo
+{
+    public class CustomHeaderMiddleware : IMiddleware
+    {
+        private readonly ILogger<CustomHeaderMiddleware> _logger;
+
+        public CustomHeaderMiddleware(ILogger<CustomHeaderMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            context.Response.Headers.Add("X-Custom-Header", "Custom Header");
+            await next(context);
+        }
+    }
+}
