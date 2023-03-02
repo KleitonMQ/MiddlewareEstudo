d857e015ab53a01f57edcd506fc04057f0bb166f
Kleiton MQ <kleiton.x5@gmail.com>
2023-03-02 11:13:42 -0300

    instacao do pacote de dependencia Microsoft.Extensions.DependencyInjection.Abstractions

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MiddlewareEstudo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<CustomHeaderMiddleware>();
 
 var app = builder.Build();
 
@@ -19,6 +22,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<CustomHeaderMiddleware>();
 
 app.MapControllers();
 
